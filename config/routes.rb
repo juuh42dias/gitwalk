85648504f9d771cc812830d36ea88463aa6f0829
Juliana Dias <hello@juliana.dev>
2023-02-06 01:06:53 -0300

    WIP: Fix rubocop suggestions

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,20 @@
-Rails.application.routes.draw do
-  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
+# frozen_string_literal: true
 
-  # Defines the root path route ("/")
-  # root "articles#index"
+require 'sidekiq/web'
+
+# Configure Sidekiq-specific session middleware
+Sidekiq::Web.use ActionDispatch::Cookies
+Sidekiq::Web.use Rails.application.config.session_store, Rails.application.config.session_options
+
+Rails.application.routes.draw do
+  mount Sidekiq::Web => '/sidekiq'
+
+  namespace :api do
+    namespace :v1 do
+      namespace :github do
+        post 'users', to: 'users#create'
+        get 'users/:username', to: 'users#show', as: :user
+      end
+    end
+  end
 end
